199e65a78ac51d235a0dfadfbc566304f35c78e2
Eduard Krivanek <47821225+krivanek06@users.noreply.github.com>
2020-11-16 18:59:07 +0100

    fixes from call

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
-import {Observable} from 'rxjs';
+import {from, Observable, of} from 'rxjs';
 import {IUser, IUserNotification} from '../../models/IUser.model';
 import {Router} from '@angular/router';
 import {filter, flatMap, map, reduce, scan, take, tap} from 'rxjs/operators';
@@ -9,62 +9,87 @@ import {DiscoveredMoviesWrapper} from '../../../api/film-data.model';
 import {IonSearchbar} from '@ionic/angular';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  user$: Observable<IUser>;
-  unreadNotifications$: Observable<number>;
-  searchedMovies$: Observable<DiscoveredMoviesWrapper>;
+    user$: Observable<IUser>;
+    unreadNotifications$: Observable<number>;
+    searchedMovies$: Observable<DiscoveredMoviesWrapper>;
 
-  @ViewChildren('mySearchbar') searchBars: QueryList<IonSearchbar>;
+    searchMovieValue = '';
 
-  constructor(private auth: AuthService,
-              private filmDataService: FilmDataService,
-              private router: Router) {
-  }
+    @ViewChildren('mySearchbar') searchBars: QueryList<IonSearchbar>;
 
-  ngOnInit(): void {
-    this.user$ = this.auth.getUser();
-
-    // sum up unread notifications
-    this.unreadNotifications$ = this.auth.getUser().pipe(
-      filter(user => !!user),
-      map(user => user.notifications
-        .filter(x => !x.read)
-        .reduce((acc, one: IUserNotification) => acc + 1, 0))
-    );
-  }
-
-  searchForMovie(data: CustomEvent) {
-    if (!data.detail.value || data.detail.value.length <= 3) {
-      return;
+    constructor(private auth: AuthService,
+                private filmDataService: FilmDataService,
+                private router: Router) {
     }
-    this.searchedMovies$ = this.filmDataService.searchMovieByName(data.detail.value);
 
-  }
 
-  signOut() {
-    this.auth.logout();
-  }
+    ngOnInit(): void {
+        this.user$ = this.auth.getUser();
 
-  redirectToLogin(type: string) {
-    this.router.navigate([`authentication/${type}`]);
-  }
+        this.checkUnreadNotification();
 
-  redirectToProfile() {
-    this.router.navigate([`menu/profile`]);
-  }
+        of(this.searchMovieValue).subscribe(console.log);
 
-  redirectToDashboard() {
-    this.router.navigate([`menu/dashboard`]);
-  }
+    }
 
-  redirectToDetails(movieId: number) {
-    this.searchBars.forEach(searchbar => searchbar.value = null);
-    this.searchedMovies$ = undefined;
-    this.router.navigate([`menu/details/${movieId}`]);
-  }
+    searchForMovie(data: CustomEvent) {
+        if (!data.detail.value || data.detail.value.length <= 3) {
+            this.searchMovieValue = data.detail.value;
+            if (this.searchMovieValue.length === 0) {
+                this.cancelSearch();
+            }
+            return;
+        }
+        if (data.detail.value === this.searchMovieValue) {
+            return;
+        }
+
+        this.searchMovieValue = data.detail.value;
+        this.searchedMovies$ = this.filmDataService.searchMovieByName(data.detail.value);
+
+    }
+
+    cancelSearch() {
+        this.searchMovieValue = '';
+        this.searchedMovies$ = undefined;
+    }
+
+    signOut() {
+        this.auth.logout();
+    }
+
+    redirectToLogin(type: string) {
+        this.router.navigate([`authentication/${type}`]);
+    }
+
+    redirectToProfile() {
+        this.router.navigate([`menu/profile`]);
+    }
+
+    redirectToDashboard() {
+        this.router.navigate([`menu/dashboard`]);
+    }
+
+    redirectToDetails(movieId: number) {
+        this.searchBars.forEach(searchbar => searchbar.value = null);
+        this.searchedMovies$ = undefined;
+        this.router.navigate([`menu/details/${movieId}`]);
+    }
+
+
+    private checkUnreadNotification() {
+        // sum up unread notifications
+        this.unreadNotifications$ = this.auth.getUser().pipe(
+            filter(user => !!user),
+            map(user => user.notifications
+                .filter(x => !x.read)
+                .reduce((acc, one: IUserNotification) => acc + 1, 0))
+        );
+    }
 
 }
